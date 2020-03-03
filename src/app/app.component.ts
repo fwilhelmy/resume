1ea0a196967336c3b07f8ee1808745f79278b664
Félix <Felix.wilhelmy.1@ens.etsmtl.ca>
2020-03-03 12:23:58 -0500

    Printable format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionService } from './shared/service/session.service';
-import data from './data.json';
+import data1 from './data.json';
+import data2 from './resources.json';
 
 @Component({
     selector: 'app-root',
@@ -8,14 +9,43 @@ import data from './data.json';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    public resume = data;
-    public language = "FR";
+    public resume = data1;
+    public resource = data2;
+    public language = 'FR';
 
     constructor() {
     }
 
     ngOnInit() {
         console.log(this.resume);
+        window.onscroll = () => this.isTop();
+    }
+
+    isTop() {
+        let scrollBtn = document.getElementById('scroll-btn');
+
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollBtn.style.display = 'block';
+        } else {
+            scrollBtn.style.display = 'none';
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    toTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+
+    lang(content) {
+        if (content) {
+            const langContent = this.language === 'FR' ? content.fr : content.en;
+            if (langContent) {
+                return langContent;
+            } else {
+                return content;
+            }
+        }
     }
 
     scroll(el: HTMLElement) {
